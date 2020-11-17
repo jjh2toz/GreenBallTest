a3a4ebea60e6ac95159397fe8745daa3193f53ed
JUNE <jjhttoz@naver.com>
2020-11-17 14:19:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/greenball/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.greenball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
