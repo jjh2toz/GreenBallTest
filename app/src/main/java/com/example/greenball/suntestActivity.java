d6cdb96e160f3b610455f238f42facd2c155fb64
ukgityes <ksug114@naver.com>
2020-11-17 14:27:32 +0900

    선욱 테스트

---
+++ app/src/main/java/com/example/greenball/suntestActivity.java
@@ -0,0 +1,15 @@
+package com.example.greenball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class suntestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suntest);
+        // 올라가라
+    }
+}
